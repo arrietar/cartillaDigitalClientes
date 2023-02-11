b6bb37349a5c72ed96650e6ee3af9b3724410851
arrietar <63684575+arrietar@users.noreply.github.com>
2023-02-10 22:15:40 -0500

    Add files via upload
    Cargue de todos los archivos del repositorio

---
+++ proyecto_cartillaDigital/src/main/java/Sistema/clientesBL/clientesBL.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Sistema.clientesBL;
+
+/**
+ *
+ * @author renea
+ */
+public class clientesBL {
+    private int ID;
+    private String Nombre;
+    private String Cargo;
+
+    /**
+     * @return the ID
+     */
+    public int getID() {
+        return ID;
+    }
+
+    /**
+     * @param ID the ID to set
+     */
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    /**
+     * @return the Nombre
+     */
+    public String getNombre() {
+        return Nombre;
+    }
+
+    /**
+     * @param Nombre the Nombre to set
+     */
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    /**
+     * @return the Cargo
+     */
+    public String getCargo() {
+        return Cargo;
+    }
+
+    /**
+     * @param Cargo the Cargo to set
+     */
+    public void setCargo(String Cargo) {
+        this.Cargo = Cargo;
+    }
+    
+    
+    
+}
